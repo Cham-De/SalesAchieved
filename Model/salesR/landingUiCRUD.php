cca8752ba253fe8578464bdde76ed77afea0dfa4
Pamudi Guruge <92705961+Pamudi00@users.noreply.github.com>
2023-05-06 20:57:01 +0530

    Delete Model directory

--- Model/salesR/landingUiCRUD.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $query = "SELECT * FROM user";
-    $userData = mysqli_query($con, $query);
-    if (mysqli_error($con)) {
-        echo "Failed to connect to MySQL: " . mysqli_error($con);
-        exit();
-    }
-?>
