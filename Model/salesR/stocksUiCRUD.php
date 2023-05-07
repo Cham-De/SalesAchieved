ecd76c472a63eddc2abe0430a6d22b5601522c8b
Masha Nilushi Pupulewatte <mashanilushi@gmail.com>
2023-05-07 13:37:36 +0530

    Solved stocksUI error

--- Model/salesR/stocksUiCRUD.php
+++ Model/salesR/stocksUiCRUD.php
@@ -4,11 +4,11 @@
     //Search bar Functionality
     if(isset($_POST['search'])){
         $productSearch = $_POST['productSearch'];
-        $query = "SELECT productCategory, product.productCode, productName, IFNULL(count - SUM(quantity), count) as availableQuantity, sellingPrice 
-                    FROM orders
+        $query = "SELECT productCategory, product.productCode, productName, (count - SUM(case when orderStatus = \"Pending\" then quantity else 0 end)) as availableQuantity, sellingPrice
+                    FROM orders 
                     INNER JOIN order_product ON orders.orderID = order_product.orderID 
-                    RIGHT JOIN product ON product.productCode = order_product.productCode 
-                    WHERE (orderStatus = 'Pending' or orderStatus is NULL) AND (productName LIKE \"%$productSearch%\" OR productCategory LIKE \"%$productSearch%\" OR product.productCode LIKE \"%$productSearch%\")
+                    RIGHT JOIN product ON product.productCode = order_product.productCode
+                    WHERE productName LIKE \"%$productSearch%\" OR productCategory LIKE \"%$productSearch%\" OR product.productCode LIKE \"%$productSearch%\"
                     GROUP BY product.productCode;";
         $result = mysqli_query($con, $query);
         if (mysqli_error($con)) {
@@ -18,11 +18,10 @@
     }
 
     else{
-        $query = "SELECT productCategory, product.productCode, productName, IFNULL(count - SUM(quantity), count) as availableQuantity, sellingPrice 
-                    FROM orders
-                        INNER JOIN order_product ON orders.orderID = order_product.orderID 
-                        RIGHT JOIN product ON product.productCode = order_product.productCode 
-                    WHERE orderStatus = 'Pending' or orderStatus is NULL
+        $query = "SELECT productCategory, product.productCode, productName, (count - SUM(case when orderStatus = \"Pending\" then quantity else 0 end)) as availableQuantity, sellingPrice
+                    FROM orders 
+                    INNER JOIN order_product ON orders.orderID = order_product.orderID 
+                    RIGHT JOIN product ON product.productCode = order_product.productCode 
                     GROUP BY product.productCode;";
         $result = mysqli_query($con, $query);
         if (mysqli_error($con)) {
