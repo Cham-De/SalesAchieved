971e3538cfd20dd886815383438d797e1ca06e5b
Masha Nilushi Pupulewatte <mashanilushi@gmail.com>
2023-05-14 22:33:11 +0530

    Completed automation backend

--- Model/fetch_api.php
+++ Model/fetch_api.php
@@ -18,10 +18,17 @@ $client->addScope(Sheets::SPREADSHEETS_READONLY);
 $service = new Sheets($client);
 
 // Get the ID of the Google Sheet you want to access
-$spreadsheetId = '1Bw8gRbFmrKynwgjXpUlAVFzJywAhXL2LYfLVMZLQqVk';
+$spreadsheetId = '1LKHOclSKZwqzim1rMNnhk9q-5oNOJV1-Vcuo6uUZy8w';
 
 // Define the range of data you want to retrieve
-$range = 'Sheet1!A2:C3';
+// $range = 'Sheet1!A2:L2';
+
+$sheetName = 'Form Responses 1'; // Replace with the name of your sheet
+
+$range = $sheetName . '!A2:L';
+// $lastRow = $service->spreadsheets_values->get($spreadsheetId, $range)->getLastRow();
+
+// $range = $sheetName . '!A2:D' . $lastRow;
 
 // Make the API request to retrieve the data
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
@@ -31,16 +38,56 @@ $values = $response->getValues();
 if (empty($values)) {
     print "No data found.\n";
 } else {
-    foreach ($values as $row) {
-        // print implode("\t", $row) . "\n";
+    $lastRow = count($values) - 1; // Calculate the last row
+    $row = $values[$lastRow];
+    // print implode("\t", $row) . "\n";
 
-        $column1 = $row[0];
-        $column2 = $row[1];
-        $column3 = $row[2];
+    $existingCustomer = $row[2];
+    $canRememberEmail = $row[3];
+    $checkEmail = $row[4];
+    $name = $row[5];
+    $phone = $row[6];
+    $address = $row[7];
+    $email = $row[8];
 
-        echo "Column 1: $column1, Column 2: $column2, Column 3: $column3 <br>";
-        $sql = "INSERT INTO sheets_data (custID, email) VALUES ('$column2', '$column3')";
+    if ($existingCustomer == "No" || $canRememberEmail == "No"){
+        $sql = "INSERT INTO customer (customerName, phone, address, email) VALUES ('$name', '$phone', '$address', '$email')";
+        mysqli_query($con, $sql);
+        $customerID = $con->insert_id;
+    }
+    else {
+        $sql = "SELECT customerID FROM customer WHERE email = '$checkEmail'";
+        $res = mysqli_query($con, $sql);
+        $customerID = $res["customerID"];
+    }
 
-        $query = mysqli_query($con, $sql);
+    $productStr = $row[9];
+    $productEntries = explode("\n", $productStr);
+    $productList = [];
+    foreach ($productEntries as $entry){
+        $productDetails = explode(" - ", $entry);
+        $productList[] = $productDetails;
+    }
+
+    $paymentMethod = $row[10] == "Bank Transfer" ? "BT" : "COD";
+    if (str_starts_with($row[11], "Within Colombo")){
+        $deliveryRegion = "Within Colombo";
+    }
+    else if (str_starts_with($row[11], "Colombo Suburbs")){
+        $deliveryRegion = "Colombo Suburbs";
+    }
+    else {
+        $deliveryRegion = "Out of Colombo";
+    }
+    
+    $sql = "INSERT INTO orders (customerID, paymentMethod, deliveryRegion) VALUES ('$customerID', '$paymentMethod', '$deliveryRegion')";
+    mysqli_query($con, $sql);
+    $orderID = $con->insert_id;
+    
+    foreach ($productList as $product){
+        $productCode = $product[0];
+        $productQuantity = $product[1];
+        $sql = "INSERT INTO order_product (orderID, productCode, quantity) VALUES ('$orderID', '$productCode', '$productQuantity')";
+        mysqli_query($con, $sql);
     }
 }
